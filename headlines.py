4c75c2f090486e5cbccc5e5d8a477507b522e34c
Himanshu Negi <himnegi1@sapenorthern.local>
2019-09-23 17:12:54 +0530

    cosmatic changes

--- headlines.py
+++ headlines.py
@@ -5,6 +5,7 @@ app=Flask(__name__)
 
 NUMBER_OF_TOP_NEWS=5
 
+# Below are RSS feed links.
 RSS_FEEDS = {
     'economy' : 'https://economictimes.indiatimes.com/news/economy/rssfeeds/1373380680.cms',
     'politics': 'https://economictimes.indiatimes.com/news/politics-and-nation/rssfeeds/1052732854.cms',
@@ -31,21 +32,18 @@ def get_news(publication="bbc"):
         first_article = feed['entries'][i]
         # print(type(feed))
         # print(feed)
-        my_string+="""
-          <a href={0}>link</a><br/>
-                    <b>{1}</b> <br />
+        my_string += """
+                    <a href={0}><b>{1}</b></a> <br />
                     <i>{2}</i> <br />
-                    <p>{3}</p> <br />
-              
-                    <br/><br/>
-                """.format(first_article.get("link"),
-                            first_article["title"],
-                            first_article.get("published"),
-                            first_article.get("summary"))
+                    <p>{3}</p> <br /><br />
+                <hr>
+                """.format(first_article.get("link"), first_article["title"],
+                           first_article.get("published"),
+                           first_article.get("summary"))
 
 
     print(my_string)
-    
+
     return """<html>
             <body>
             """ + my_string + """
