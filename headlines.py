13b7af8af41317d53954d6a1c1e53cf792ccbdce
Himanshu Negi <himnegi1@sapenorthern.local>
2019-09-24 14:25:41 +0530

    POST requests

--- headlines.py
+++ headlines.py
@@ -18,9 +18,9 @@ RSS_FEEDS = {
     'tech'    : 'https://economictimes.indiatimes.com/industry/tech/rssfeeds/56811438.cms'
 }
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def get_news():
-    query=request.args.get("publication")
+    query=request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication="tech"
     else:
